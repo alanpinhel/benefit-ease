6794de7e7b67bcabd0fa57e94190c64d59b945cc
Alan Pinhel <alanpinhel@gmail.com>
2024-04-14 18:14:48 -0300

    fix: type

--- apps/web/mocks/handlers.ts
+++ apps/web/mocks/handlers.ts
@@ -1,6 +1,6 @@
-import { http, HttpResponse } from "msw";
+import { http, HttpResponse, RequestHandler } from "msw";
 
-export const handlers = [
+export const handlers: RequestHandler[] = [
   http.post("*/auth/v1/signup", () =>
     HttpResponse.json({ id: "225b1d6f-4032-410a-8c24-33fc27cf7140" })
   ),
