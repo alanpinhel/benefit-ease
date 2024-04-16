f3d1818b4ac3671d893aadda0d398185eafccd00
Alan Pinhel <alanpinhel@gmail.com>
2024-04-16 17:10:55 -0300

    feat: adiciona configuracao padrao para cookies

---
+++ apps/web/lib/cookies.ts
@@ -0,0 +1,12 @@
+import { Cookies } from "react-cookie";
+
+const domain = () =>
+  typeof window !== "undefined"
+    ? window.location.hostname.split(".").slice(-2).join(".")
+    : "";
+
+export const cookies = new Cookies(undefined, {
+  domain: domain(),
+  path: "/",
+  secure: process.env.NODE_ENV === "production",
+});
