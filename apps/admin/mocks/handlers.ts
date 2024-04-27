06c8ae213dd1f08e592eafd1096d615bdc57e5ec
Alan Pinhel <alanpinhel@gmail.com>
2024-04-27 12:50:17 -0300

    fix: imports

--- apps/admin/mocks/handlers.ts
+++ apps/admin/mocks/handlers.ts
@@ -1,63 +1,10 @@
-import { Account, Transaction } from "@/app/page";
 import { HttpResponse, RequestHandler, http } from "msw";
 
 export const handlers: RequestHandler[] = [
   http.get("*/rest/v1/accounts", () => {
-    return HttpResponse.json([
-      {
-        id: 1,
-        balance: 840,
-        benefits: {
-          id: 1,
-          name: "Alimentação",
-          color_from: "teal",
-          color_to: "blue",
-          icon: "🥦",
-        },
-      },
-      {
-        id: 2,
-        balance: 360,
-        benefits: {
-          id: 2,
-          name: "Mobilidade",
-          color_from: "lime",
-          color_to: "green",
-          icon: "🚘",
-        },
-      },
-    ] satisfies Account[]);
+    return HttpResponse.json([]);
   }),
   http.get("*/rest/v1/transactions", () => {
-    return HttpResponse.json([
-      {
-        id: 1,
-        amount: -212.23,
-        name: "Supermercado",
-        created_at: "2024-04-22T09:00:00",
-        accounts: { benefits: { icon: "🥦" } },
-      },
-      {
-        id: 2,
-        amount: -29.21,
-        name: "Uber",
-        created_at: "2024-04-21T09:00:00",
-        accounts: { benefits: { icon: "🚘" } },
-      },
-      {
-        id: 3,
-        amount: -14.15,
-        name: "Uber",
-        created_at: "2024-04-15T09:00:00",
-        accounts: { benefits: { icon: "🚘" } },
-      },
-      {
-        id: 4,
-        amount: -12,
-        name: "Uber",
-        created_at: "2024-04-08T09:00:00",
-        accounts: { benefits: { icon: "🚘" } },
-      },
-    ] satisfies Transaction[]);
+    return HttpResponse.json([]);
   }),
 ];
--- packages/components/src/without-auth.tsx
+++ packages/components/src/without-auth.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 
 export function withoutAuth(Component: any) {
   return function WithoutAuth(props: any) {
-    const router = useRouter();
     const [hasMounted, setHasMounted] = useState(false);
     const [cookies] = useCookies(["access_token"]);
 
     useEffect(() => {
       setHasMounted(true);
       if (cookies.access_token) {
-        router.push("/");
+        window.location.href = `${process.env.NEXT_PUBLIC_WEB_URL}/`;
       }
     }, [cookies.access_token]);
 
