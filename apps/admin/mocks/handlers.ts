718f479cf14989e88a75a444d4f185524aeef357
Alan Pinhel <alanpinhel@gmail.com>
2024-04-27 16:12:03 -0300

    feat(mocks): cria pacote para reutilizar mocks

--- apps/admin/mocks/handlers.ts
+++ apps/admin/mocks/handlers.ts
@@ -1,10 +1,11 @@
+import { accounts, transactions } from "@repo/mocks";
 import { HttpResponse, RequestHandler, http } from "msw";
 
 export const handlers: RequestHandler[] = [
   http.get("*/rest/v1/accounts", () => {
-    return HttpResponse.json([]);
+    return HttpResponse.json(accounts);
   }),
   http.get("*/rest/v1/transactions", () => {
-    return HttpResponse.json([]);
+    return HttpResponse.json(transactions);
   }),
 ];
