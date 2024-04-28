779f1babc72ceb50c859acd5d4fd4a0312d2ea5d
Alan Pinhel <alanpinhel@gmail.com>
2024-04-28 17:02:00 -0300

    test(admin): exibe botão e menu para criar nova conta

--- apps/admin/app/page.test.tsx
+++ apps/admin/app/page.test.tsx
@@ -1,4 +1,14 @@
-import { createAuthEnvironment, render, screen, within } from "@/test-utils";
+import { benefits } from "@/mocks/handlers";
+import { server } from "@/mocks/node";
+import {
+  createAuthEnvironment,
+  render,
+  screen,
+  userEvent,
+  waitForElementToBeRemoved,
+  within,
+} from "@/test-utils";
+import { http } from "msw";
 import HomePage from "./page";
 
 createAuthEnvironment();
@@ -6,7 +16,9 @@ createAuthEnvironment();
 test("shows benefit accounts when accessing home", async () => {
   render(<HomePage />);
 
-  expect(await screen.findByText("Contas")).toBeInTheDocument();
+  await waitForElementToBeRemoved(() =>
+    screen.getByText(/carregando contas.../i)
+  );
 
   const accountEl1 = screen.getByTestId("account-1");
   expect(within(accountEl1).getByText("🥦")).toBeInTheDocument();
@@ -17,4 +29,57 @@ test("shows benefit accounts when accessing home", async () => {
   expect(within(accountEl2).getByText("🚘")).toBeInTheDocument();
   expect(within(accountEl2).getByText("R$ 360,00")).toBeInTheDocument();
   expect(within(accountEl2).getByText("Mobilidade")).toBeInTheDocument();
+});
+
+test("shows add account button when there is still benefit available", async () => {
+  render(<HomePage />);
+
+  await waitForElementToBeRemoved(() =>
+    screen.getByText(/carregando contas.../i)
+  );
+
+  expect(screen.getByText(/adicionar conta/i)).toBeInTheDocument();
+});
+
+test("does not show the add account button when it no longer contains available benefits", async () => {
+  server.use(
+    http.get("*/rest/v1/benefits", () => Response.json(benefits.slice(0, 2)))
+  );
+
+  render(<HomePage />);
+
+  await waitForElementToBeRemoved(() =>
+    screen.getByText(/carregando contas.../i)
+  );
+
+  expect(screen.queryByText(/adicionar conta/i)).not.toBeInTheDocument();
+});
+
+test("shows add account button disabled when fetch error occurs", async () => {
+  server.use(
+    http.get("*/rest/v1/benefits", () =>
+      Response.json({ message: "Request rate limit exceeded" }, { status: 429 })
+    )
+  );
+
+  render(<HomePage />);
+
+  await waitForElementToBeRemoved(() =>
+    screen.getByText(/carregando contas.../i)
+  );
+
+  const button = screen.getByRole("button", { name: /adicionar conta/i });
+  expect(button).toBeDisabled();
+  await userEvent.hover(button);
+  expect(await screen.findByText(/adição indisponível/i)).toBeInTheDocument();
+});
+
+// This test should be executed because it will log out the user
+test("logs the person out when they click log out", async () => {
+  render(<HomePage />);
+
+  await userEvent.click(screen.getByRole("button", { expanded: false }));
+  userEvent.click(await screen.findByRole("menuitem", { name: /sair/i }));
+
+  await waitForElementToBeRemoved(() => screen.getByText(/john doe/i));
 });
--- apps/admin/mocks/handlers.ts
+++ apps/admin/mocks/handlers.ts
@@ -1,5 +1,12 @@
+import { Benefit } from "@/app/page";
 import { accounts, transactions } from "@repo/mocks";
 import { HttpResponse, RequestHandler, http } from "msw";
+
+export const benefits: Benefit[] = [
+  { id: 1, name: "Alimentação", icon: "🥦" },
+  { id: 2, name: "Mobilidade", icon: "🚘" },
+  { id: 3, name: "Saúde", icon: "🩺" },
+];
 
 export const handlers: RequestHandler[] = [
   http.get("*/rest/v1/accounts", () => {
@@ -8,7 +15,13 @@ export const handlers: RequestHandler[] = [
   http.delete("*/rest/v1/accounts", () => {
     return HttpResponse.json({});
   }),
+  http.post("*/rest/v1/accounts", () => {
+    return HttpResponse.json({});
+  }),
   http.get("*/rest/v1/transactions", () => {
     return HttpResponse.json(transactions);
+  }),
+  http.get("*/rest/v1/benefits", () => {
+    return HttpResponse.json(benefits);
   }),
 ];
