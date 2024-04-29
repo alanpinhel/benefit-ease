932c2061da976746078a4cb6e4f5e9daac55f6fb
Alan Pinhel <alanpinhel@gmail.com>
2024-04-29 12:38:11 -0300

    test(admin): lista transacoes na pagina de admin

--- apps/admin/app/account/[id]/page.test.tsx
+++ apps/admin/app/account/[id]/page.test.tsx
@@ -4,20 +4,43 @@ import {
   render,
   screen,
   userEvent,
+  waitForElementToBeRemoved,
+  within,
 } from "@/test-utils";
 import AccountPage from "./page";
 
 createAuthEnvironment();
 
-test("delete account when click delete button", async () => {
+function renderAccountPage() {
   render(
     <PathParamsProviderMock params={{ id: "1" }}>
       <AccountPage />
     </PathParamsProviderMock>
   );
+}
 
-  await userEvent.click(await screen.findByText("Excluir conta"));
-  userEvent.click(await screen.findByText("Deletar conta"));
+test("delete account when click delete button", async () => {
+  renderAccountPage();
+
+  await waitForElementToBeRemoved(() =>
+    screen.getByText(/carregando conta.../i)
+  );
+
+  await userEvent.click(screen.getByText(/excluir conta/i));
+  await userEvent.click(await screen.findByText(/deletar conta/i));
 
   expect(await screen.findByText(/conta excluída/i)).toBeInTheDocument();
+});
+
+test("shows transactions for the selected account", async () => {
+  renderAccountPage();
+
+  await waitForElementToBeRemoved(() =>
+    screen.getByText(/carregando conta.../i)
+  );
+
+  const el1 = await screen.findByTestId("transaction-1");
+  expect(within(el1).getByText("Supermercado")).toBeInTheDocument();
+  expect(within(el1).getByText("22/04/2024 09:00")).toBeInTheDocument();
+  expect(within(el1).getByText("R$ -212,23")).toBeInTheDocument();
 });
